a6ada1679f5bbaa782d687210d7a0a813891d66e
brunosastre1 <bruno.sastre@icloud.com>
2016-05-11 14:13:10 -0300

    inclusao do logo

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -1,10 +1,46 @@
-#include <cstdlib>
-#include <iostream>
+ /**************************************************************************************************************************
+ * Programa: Kruskal	
+ * 
+ * Descriçao:  Programa que implementa o algoritmo de Kruskall para resolver o "Problema da árvore geradora de custo mínimo". 
+ *
+ * Autores:  Bruno Sastre     Felipe Aversoni
+ * TIA:      41316861         41313038 
+ ****************************************************************************************************************************/
 
-using namespace std;
+#include<stdlib.h>
+#include<stdio.h>
+#include<conio.h>
 
-int main(int argc, char *argv[])
-{
-    system("PAUSE");
-    return EXIT_SUCCESS;
+#define TAM_MAXIMO 128
+
+void imprimir_logo(FILE *impArq);
+
+int main() {
+ 
+    //Impressao logo  
+    char *nome_arquivo = "imagem.txt";
+    FILE *impArq = NULL;
+ 
+    if((impArq = fopen(nome_arquivo,"r")) == NULL)
+    {
+        fprintf(stderr,"erro ao abrir %s\n",nome_arquivo);
+        return 1;
+    }
+    
+    imprimir_logo(impArq);
+    printf("\n");
+    fclose(impArq);
+    printf("\n");
+    
+    
+
+	system("pause");
 }
+
+    void imprimir_logo(FILE *impArq){
+    
+    char read_string[TAM_MAXIMO];
+    
+    while(fgets(read_string,sizeof(read_string),impArq) != NULL)
+        printf("%s",read_string);
+    }
